04e9453fb1b2436055036380003deb483d420a14
Hitkul <hitkuljangid@gmail.com>
2018-10-01 14:53:10 +0530

    pulling links to template

--- scraper.py
+++ scraper.py
@@ -4,72 +4,94 @@ import requests
 import shutil
 import os.path
 
-save_path = 'memes'
-n_captions = 2   #this number *15 is the total number of captions per template
+meme_template_path = 'memes'
+n_captions = 1   #this number *15 is the total number of captions per template
 n_templates = 2  #this number *15 is the total number of templates
-Uerrors = 0
 
-for i in range(1,n_templates):
-    if i == 1:
-        url = 'https://memegenerator.net/memes/popular/alltime'
-    else:
-        url = 'https://memegenerator.net/memes/popular/alltime/page/' + str(i)
+def get_meme_templates(n_templates):
+    links = []
+    imgs=[]
+    for i in range(1,n_templates):
+        if i == 1:
+            url = 'https://memegenerator.net/memes/popular/alltime'
+        else:
+            url = 'https://memegenerator.net/memes/popular/alltime/page/' + str(i)
 
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text,'html.parser')
-    chars = soup.find_all(class_='char-img')
-    links = [char.find('a') for char in chars]
-    imgs = [char.find('img') for char in chars]
-    assert len(links) == len(imgs)
-    for j,img in enumerate(imgs):
-        img_url = img['src']
-        response = requests.get(img_url, stream=True)
-        name_of_file = img_url.split('/')[-1]
-        completeName = os.path.join(save_path, name_of_file)
-        with open(completeName,'wb') as out_file:
-            shutil.copyfileobj(response.raw, out_file)
-        del response
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text,'html.parser')
+        chars = soup.find_all(class_='char-img')
+        for char in chars:
+            links.append(char.find('a')['href'])
+            imgs.append((char.find('img')['src'],char.find('img')['alt']))
+        return links,imgs
+    
 
-        for k in range(1,n_captions):
-            if k == 1:
-                URL = 'https://memegenerator.net' + links[j]['href']
-            else:
-                URL = 'https://memegenerator.net' + links[j]['href'] + '/images/popular/alltime/page/' + str(k)
+template_links, template_imgs = get_meme_templates(n_templates)
 
-            R = requests.get(URL)
-            SOUP = BeautifulSoup(R.text,'html.parser')
-            CHARS = SOUP.find_all(class_='char-img')
-            IMGS = [char.find('img') for char in CHARS]
-            with open('Captions.txt', 'a') as f:
-                for IMG in IMGS:
-                    try:
-                        print(IMG)
-                        f.write('%s\n' % (IMG['alt']))
-                    except UnicodeEncodeError:
-                        Uerrors += 1
-                        pass
-    if i % 10 == 0:
-        print('<'+'='*10+'>')
-        print(i)
-        print(Uerrors)
+print(len(template_links),len(template_imgs))
 
 
+# for i in range(1,n_templates):
+#     if i == 1:
+#         url = 'https://memegenerator.net/memes/popular/alltime'
+#     else:
+#         url = 'https://memegenerator.net/memes/popular/alltime/page/' + str(i)
 
-URL = 'https://memegenerator.net' + links[0]['href']
-R = requests.get(URL)
-SOUP = BeautifulSoup(R.text,'html.parser')
-CHARS = SOUP.find_all(class_='char-img')
-IMGS = [char.find('img') for char in CHARS]
-print(IMGS[1]['alt'])
+#     r = requests.get(url)
+#     soup = BeautifulSoup(r.text,'html.parser')
+#     chars = soup.find_all(class_='char-img')
+#     links = [char.find('a') for char in chars]
+#     imgs = [char.find('img') for char in chars]
+#     assert len(links) == len(imgs)
+#     for j,img in enumerate(imgs):
+#         img_url = img['src']
+#         response = requests.get(img_url, stream=True)
+#         name_of_file = img_url.split('/')[-1]
+#         completeName = os.path.join(save_path, name_of_file)
+#         with open(completeName,'wb') as out_file:
+#             shutil.copyfileobj(response.raw, out_file)
+#         del response
+
+#         for k in range(1,n_captions):
+#             if k == 1:
+#                 URL = 'https://memegenerator.net' + links[j]['href']
+#             else:
+#                 URL = 'https://memegenerator.net' + links[j]['href'] + '/images/popular/alltime/page/' + str(k)
+
+#             R = requests.get(URL)
+#             SOUP = BeautifulSoup(R.text,'html.parser')
+#             CHARS = SOUP.find_all(class_='char-img')
+#             IMGS = [char.find('img') for char in CHARS]
+#             with open('Captions.txt', 'a') as f:
+#                 for IMG in IMGS:
+#                     try:
+#                         print(IMG)
+#                         f.write('%s\n' % (IMG['alt']))
+#                     except UnicodeEncodeError:
+#                         Uerrors += 1
+#                         pass
+#     if i % 10 == 0:
+#         print('<'+'='*10+'>')
+#         print(i)
+#         print(Uerrors)
 
 
-img_url = imgs[0]['src']
-response = requests.get(img_url, stream=True)
-name_of_file = img_url.split('/')[-1]
-completeName = os.path.join(save_path, name_of_file)
-with open(completeName,'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
-del response
+
+# URL = 'https://memegenerator.net' + links[0]['href']
+# R = requests.get(URL)
+# SOUP = BeautifulSoup(R.text,'html.parser')
+# CHARS = SOUP.find_all(class_='char-img')
+# IMGS = [char.find('img') for char in CHARS]
+# print(IMGS[1]['alt'])
+
+
+# img_url = imgs[0]['src']
+# response = requests.get(img_url, stream=True)
+# name_of_file = img_url.split('/')[-1]
+# completeName = os.path.join(save_path, name_of_file)
+# with open(completeName,'wb') as out_file:
+#     shutil.copyfileobj(response.raw, out_file)
+# del response
 
 
 
